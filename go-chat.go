287da4f4667efcbfe6d8d51dadc6165c43d47dbe
shunsukw <ww.shunsuke@gmail.com>
2018-08-28 17:09:31 +0900

    implemented signup function

--- go-chat.go
+++ go-chat.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	ghandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
+	"github.com/shunsukw/go-chat/common"
+	"github.com/shunsukw/go-chat/common/datastore"
 	"github.com/shunsukw/go-chat/handlers"
 	"github.com/shunsukw/go-chat/middleware"
 )
@@ -17,21 +20,23 @@ const (
 )
 
 func main() {
-	// db, err := datastore.NewDatastore(datastore.MYSQL, "gochat:gochat@/gochatdb")
+	db, err := datastore.NewDatastore(datastore.MYSQL, "gochat:gochat@/gochatdb")
 	//db, err := datastore.NewDatastore(datastore.MONGODB, "localhost:27017")
 	//db, err := datastore.NewDatastore(datastore.REDIS, "localhost:6379")
-	/*
-		if err != nil {
-			log.Print(err)
-		}
-		defer db.Close()
+	if err != nil {
+		log.Print(err)
+	}
+	defer db.Close()
 
-		e := common.Env{DB: db}
-	*/
+	env := common.Env{DB: db}
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handlers.HomeHandler)
 	// Signin Login
+	r.Handle("/signin", handlers.SignupHandler(&env)).Methods("GET", "POST")
+	r.Handle("/login", handlers.LoginHandler(&env)).Methods("GET", "POST")
+	r.HandleFunc("/logut", handlers.LogoutHandler()).Methods("GET", "POST")
 
 	// routes
 	r.Handle("/feed", http.HandlerFunc(handlers.FeedHandler)).Methods("GET")
---
+++ handlers/signup.go
@@ -0,0 +1,126 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/shunsukw/go-chat/common"
+	"github.com/shunsukw/go-chat/models"
+	"github.com/shunsukw/go-chat/validationkit"
+)
+
+// SignupForm struct ...
+type SignupForm struct {
+	PageTitle  string
+	FieldNames []string
+	Fields     map[string]string
+	Erorrs     map[string]string
+}
+
+// SignupHandler ...
+func SignupHandler(env *common.Env) http.Handler {
+	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		s := SignupForm{}
+		s.FieldNames = []string{"username", "firstName", "lastName", "email"}
+		s.Fields = make(map[string]string)
+		s.Errors = make(map[string]string)
+		s.PageTitle = "Sign Up"
+
+		switch r.Method {
+		case "GET":
+			DisplaySignupForm(w, r, &s)
+		case "POST":
+			ValidateSignupForm(w, r, &s, env)
+		default:
+			DisplaySignupForm(w, r, &s)
+		}
+	})
+}
+
+// DisplaySignupForm ...
+func DisplaySignupForm(w http.ResponseWriter, r *http.Request, s *SignupForm) {
+	RenderTemplate(w, "./templates/signupform.html", s)
+}
+
+// -------------------------------------------------
+
+// ValidateSignupForm ...
+func ValidateSignupForm(w http.ResponseWriter, r *http.Request, s *SignupForm, env *common.Env) {
+	PopulateFormFields(r, s)
+
+	if r.FormValue("username") == "" {
+		s.Errors["usernameError"] = "The username field is required."
+	}
+
+	if r.FormValue("firstName") == "" {
+		s.Errors["firstNameError"] = "The first name field is required."
+	}
+
+	if r.FormValue("lastName") == "" {
+		s.Errors["lastNameError"] = "The last name field is required."
+	}
+
+	if r.FormValue("email") == "" {
+		s.Errors["emailError"] = "The e-mail address field is required."
+	}
+
+	if r.FormValue("password") == "" {
+		s.Errors["passwordError"] = "The password field is required."
+	}
+
+	if r.FormValue("confirmPassword") == "" {
+		s.Errors["confirmPasswordError"] = "The confirm password field is required."
+	}
+
+	if validationlit.CheckUsernameSyntax(r.FormValue("username")) == false {
+		usernameErrorMessage := "The username entered has an improper syntax."
+		if _, ok := s.Errors["usernameError"]; ok {
+			s.Errors["usernameError"] += " " + usernameErrorMessage
+		} else {
+			s.Errors["usernameError"] = usernameErrorMessage
+		}
+	}
+
+	if validationkit.CheckEmailSyntax(r.FormValue("email")) == false {
+		emailErrorMessage := "The e-mail address entered has an improper syntax."
+		if _, ok := s.Errors["usernameError"]; ok {
+			s.Errors["emailError"] += " " + emailErrorMessage
+		} else {
+			s.Errors["emailError"] = emailErrorMessage
+		}
+	}
+
+	if r.FormValue("password") != r.FormValue("confirmPassword") {
+		s.Errors["confirmPasswordError"] = "The password and confirm pasword fields do not match."
+	}
+
+	if len(s.Errors) > 0 {
+		DisplaySignUpForm(w, r, s)
+	} else {
+		ProcessSignUpForm(w, r, s, e)
+	}
+}
+
+func PopulateFormFields(r *http.Request, s *SignupForm) {
+	for _, fieldname := range s.FieldNames {
+		s.Fields[fieldname] = r.FormValue(fieldname)
+	}
+}
+
+func ProcessSignupForm(w http.ResponseWriter, r *http.Request, s *SignupForm, env *common.Env) {
+	u := models.NewUser(r.FormValue("username"), r.FormValue("firstName"), r.FormValue("lastName"), r.FormValue("email"), r.FormValue("password"))
+	err := env.DB.CreateUser(&u)
+	if err != nil {
+		log.Print(err)
+	}
+
+	// Display form confirmation message
+	DisplayConfirmation(w, r, s)
+}
+
+// --------------------------------------------------
+
+// DisplayComfirmation ...
+func DisplayConfirmation(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
+	RenderTemplate(w, "./templates/signupconfirmation.html", s)
+}
