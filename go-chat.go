61004c03d42815833a01474e24b86897ae22ec94
shunsukw <ww.shunsuke@gmail.com>
2018-08-30 14:04:35 +0900

    implemented login middleware

--- go-chat.go
+++ go-chat.go
@@ -19,7 +19,7 @@ const (
 	WEBSERVERPORT = ":8443"
 )
 
-// TODO: middleware / slt / asyncq
+// TODO: slt / asyncq
 
 func main() {
 	db, err := datastore.NewDatastore(datastore.MYSQL, "gochat:gochat@/gochatdb")
@@ -38,17 +38,17 @@ func main() {
 	// Signin Login
 	r.Handle("/signin", handlers.SignupHandler(&env)).Methods("GET", "POST")
 	r.Handle("/login", handlers.LoginHandler(&env)).Methods("GET", "POST")
-	r.HandleFunc("/logut", handlers.LogoutHandler()).Methods("GET", "POST")
+	r.HandleFunc("/logut", handlers.LogoutHandler).Methods("GET", "POST")
 
 	// routes
-	r.Handle("/feed", http.HandlerFunc(handlers.FeedHandler)).Methods("GET")
-	r.Handle("/friends", http.HandlerFunc(handlers.FriendsHandler)).Methods("GET,POST")
-	r.Handle("/find", http.HandlerFunc(handlers.FindHandler)).Methods("GET")
-	r.Handle("/profile", http.HandlerFunc(handlers.MyProfileHandler)).Methods("GET")
-	r.Handle("/profile/{username}", http.HandlerFunc(handlers.ProfileHandler)).Methods("GET")
-	r.Handle("/postpreview", http.HandlerFunc(handlers.PostPreviewHandler)).Methods("GET", "POST")
-	r.Handle("/upload-image", http.HandlerFunc(handlers.UploadImageHandler)).Methods("GET", "POST")
-	r.Handle("/upload-video", http.HandlerFunc(handlers.UploadVideoHandler)).Methods("GET", "POST")
+	r.Handle("/feed", middleware.GatedContentHandler(handlers.FeedHandler)).Methods("GET")
+	r.Handle("/friends", middleware.GatedContentHandler(handlers.FriendsHandler)).Methods("GET,POST")
+	r.Handle("/find", middleware.GatedContentHandler(handlers.FindHandler)).Methods("GET")
+	r.Handle("/profile", middleware.GatedContentHandler(handlers.MyProfileHandler)).Methods("GET")
+	r.Handle("/profile/{username}", middleware.GatedContentHandler(handlers.ProfileHandler)).Methods("GET")
+	r.Handle("/postpreview", middleware.GatedContentHandler(handlers.PostPreviewHandler)).Methods("GET", "POST")
+	r.Handle("/upload-image", middleware.GatedContentHandler(handlers.UploadImageHandler)).Methods("GET", "POST")
+	r.Handle("/upload-video", middleware.GatedContentHandler(handlers.UploadVideoHandler)).Methods("GET", "POST")
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
--- middleware/gated.go
+++ middleware/gated.go
@@ -16,5 +16,25 @@ func GatedContentHandler(next http.HandlerFunc) http.Handler {
 		if err != nil {
 			log.Print(err)
 		}
+
+		if _, ok := secureCookieMap["sid"]; ok == true {
+			gfSession, err := authenticate.SessionStore.Get(r, "gopherface-session")
+			if err != nil {
+				log.Print(err)
+				return
+			}
+
+			if gfSession.Values["SessionID"] == secureCookieMap["sid"] && gfSession.Values["username"] == secureCookieMap["sid"] {
+				next(w, r)
+			} else {
+				shouldRedirectToLogin = true
+			}
+		} else {
+			shouldRedirectToLogin = true
+		}
+
+		if shouldRedirectToLogin == true {
+			http.Redirect(w, r, "/login", 302)
+		}
 	})
 }
